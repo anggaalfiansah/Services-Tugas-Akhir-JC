d8f8215b05246b3496eb155e4951ec6abeb0bb66
anggaalfiansah <anggaalfiansah26@gmail.com>
2021-04-06 03:53:35 +0700

    Tambahan Check Out

--- controllers/attendace.controller.js
+++ controllers/attendace.controller.js
@@ -40,12 +40,41 @@ exports.CheckIn = async (req, res) => {
     if (check.CheckIn === null) {
       await AttendanceData.updateOne(filter, update).then(() => {
         res.status(200).json({
-          message: `Check In berhasil pada ${update.CheckIn}`,
+          message: `Selamat datang ${check.Nama}, check In berhasil pada ${update.CheckIn}`,
         });
       });
     } else {
       res.status(200).json({
         message: `Anda sudah check in pada ${update.CheckIn}`,
+      });
+    }
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Error");
+  }
+};
+
+exports.CheckOut = async (req, res) => {
+  const { UserID, Tanggal, Bulan, Tahun, CheckOut } = req.body;
+  try {
+    const filter = { UserID, Tanggal, Bulan, Tahun };
+    const update = { CheckOut };
+    const check = await AttendanceData.findOne(filter);
+    console.log(check);
+    if (check.CheckIn !== null && check.CheckOut === null) {
+      await AttendanceData.updateOne(filter, update).then(() => {
+        res.status(200).json({
+          message: `Terimakasih atas kunjungannya, check out berhasil pada ${update.CheckOut}`,
+        });
+      });
+    } else if(check.CheckIn === null){
+        res.status(403).json({
+            message: "Anda belum check in, silahkan check in dulu",
+          });
+    }
+    else {
+      res.status(200).json({
+        message: `Anda sudah check out pada ${update.CheckOut}`,
       });
     }
   } catch (err) {
--- routes/attendance.route.js
+++ routes/attendance.route.js
@@ -5,5 +5,6 @@ var controller = require("../controllers/attendace.controller");
 /* GET home page. */
 router.post('/', controller.createAttendance);
 router.put('/CheckIn', controller.CheckIn);
+router.put('/CheckOut', controller.CheckOut);
 
 module.exports = router;
