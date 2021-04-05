252ef60275bb2025e55a8428b13129288fac5fba
anggaalfiansah <anggaalfiansah26@gmail.com>
2021-04-06 04:11:24 +0700

    update

--- controllers/attendace.controller.js
+++ controllers/attendace.controller.js
@@ -5,23 +5,36 @@ exports.createAttendance = async (req, res) => {
   try {
     const Attendance = await AttendanceData.find({ Tanggal, Bulan, Tahun });
     if (Attendance.length < 15) {
-      const data = new AttendanceData({
+      const Attendance2 = await AttendanceData.findOne({
         UserID,
         Nama,
         Tanggal,
         Bulan,
         Tahun,
-        Keterangan,
       });
-      await data.save().then(() => {
-        res.status(200).json({
-          data,
-          message: "Request berhasil terkirim",
+      if (!Attendance2) {
+        const data = new AttendanceData({
+          UserID,
+          Nama,
+          Tanggal,
+          Bulan,
+          Tahun,
+          Keterangan,
         });
-      });
+        await data.save().then(() => {
+          res.status(200).json({
+            data,
+            message: `Pengajuan kunjungan untuk ${Tanggal} ${Bulan} ${Tahun} diterima`,
+          });
+        });
+      } else {
+        res.status(403).json({
+          message: `Anda sudah mengajukan kunjungan pada ${Tanggal} ${Bulan} ${Tahun}`,
+        });
+      }
     } else {
-      res.status(200).json({
-        message: "Kuota kunjungan sudah penuh",
+      res.status(403).json({
+        message: `Kuota kunjungan pada ${Tanggal} ${Bulan} ${Tahun} sudah penuh`,
       });
     }
   } catch (err) {
@@ -36,7 +49,6 @@ exports.CheckIn = async (req, res) => {
     const filter = { UserID, Tanggal, Bulan, Tahun };
     const update = { CheckIn };
     const check = await AttendanceData.findOne(filter);
-    console.log(check);
     if (check.CheckIn === null) {
       await AttendanceData.updateOne(filter, update).then(() => {
         res.status(200).json({
@@ -60,19 +72,17 @@ exports.CheckOut = async (req, res) => {
     const filter = { UserID, Tanggal, Bulan, Tahun };
     const update = { CheckOut };
     const check = await AttendanceData.findOne(filter);
-    console.log(check);
     if (check.CheckIn !== null && check.CheckOut === null) {
       await AttendanceData.updateOne(filter, update).then(() => {
         res.status(200).json({
           message: `Terimakasih atas kunjungannya, check out berhasil pada ${update.CheckOut}`,
         });
       });
-    } else if(check.CheckIn === null){
-        res.status(403).json({
-            message: "Anda belum check in, silahkan check in dulu",
-          });
-    }
-    else {
+    } else if (check.CheckIn === null) {
+      res.status(403).json({
+        message: "Anda belum check in, silahkan check in dulu",
+      });
+    } else {
       res.status(200).json({
         message: `Anda sudah check out pada ${update.CheckOut}`,
       });
